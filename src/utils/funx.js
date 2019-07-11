763eed3e6bac2d2f2a52fd955d893c05d1bde845
Sergey.Matviychuk <sergey.matviychuk@x5.ru>
2019-07-11 21:17:39 +0300

    filter for object

--- src/utils/funx.js
+++ src/utils/funx.js
@@ -7,3 +7,13 @@ export function take(url) {
 export function showError(msg, error) {
   console.error(msg, error);
 }
+
+// expanding Object class with not-heritable method 'filter', which acts just like Array's filter
+// !!! just for fun, not for production !!!
+Object.filter = (obj, predicate) =>
+  Object.keys(obj)
+    .filter(key => predicate(obj[key]))
+    .reduce((filtered, key) => {
+      filtered[key] = obj[key];
+      return filtered;
+    }, {});
